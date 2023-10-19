b7a0b01e41d093c63767403a677df603db9baddd
Prity Das Bornita <114295389+prityd825@users.noreply.github.com>
2023-10-19 22:23:32 +0600

    Add files via upload

---
+++ Right_there.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+    int t;
+    cin>> t;
+    while(t--)
+    {
+        int n,x;
+        cin>> n>>x;
+        if(x>=n)
+        {
+            cout<<"YES"<<endl;
+        }else {
+           cout<<"NO"<<endl;
+        }
+    }
+}
