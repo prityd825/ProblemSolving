4d70af82c84203a99eccf400451a6f1255e483ed
Prity Das <prityd825@gmail.com>
2023-11-06 23:40:11 +0600

    initial commit

---
+++ Sum of two number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>> t;
+    while(t--){
+
+    int a,b;
+    cin>> a>>b;
+    int sum =0;
+    sum = a+b;
+    cout<<sum<<endl;
+    }
+}
