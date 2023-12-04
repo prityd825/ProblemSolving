acd0fc65ab5d5ba973360dec5a1ecec072176f8a
Prity Das <prityd825@gmail.com>
2023-12-04 15:40:17 +0600

    initial commit

---
+++ Roman to integer.cpp
@@ -0,0 +1,27 @@
+Problem_link: https://leetcode.com/problems/roman-to-integer/
+class Solution {
+public:
+    int romanToInt(string s) {
+        std:: unordered_map<char,int> romanValues = {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+
+        int result = 0;
+
+        for(int i=0;i<s.length();i++){
+            if(i<s.length()-1 && romanValues[s[i]]< romanValues[s[i+1]]){
+                result -= romanValues[s[i]];
+            } else {
+                result += romanValues[s[i]];
+            }
+        }
+        return result;
+
+    }
+};
