0382fb3c9e1e36becb20f3aebc3c58ae87c5f9fa
Prity Das <prityd825@gmail.com>
2023-12-18 21:59:30 +0600

    initial commit

---
+++ Maximum product difference between two pairs.cpp
@@ -0,0 +1,10 @@
+Problem_link: https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]);
+
+    }
+};
