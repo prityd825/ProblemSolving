a9ce5e4e38e0d5af42aae7db81abe77ae8a8edd5
Prity Das <prityd825@gmail.com>
2024-01-01 11:59:24 +0600

    initial commit

---
+++ Assign Cookies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int n = g.size();
+        int m = s.size();
+        int count = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < n && j < m) {
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return count;
+    }
+};
