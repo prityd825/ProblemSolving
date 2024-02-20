12fba9d3d1a0da36d7ddd3225b691300ed49c5ad
Prity Das <prityd825@gmail.com>
2024-02-20 11:47:15 +0600

    initial commit

---
+++ 231.Power of Two.cpp
@@ -0,0 +1,15 @@
+Problem_link: https://leetcode.com/problems/power-of-two/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       if(n==0)return false;
+        while(n>0){
+            if(n==1)
+                return true;
+            if(n %2 !=0)
+                break;
+            n /=2;
+        }
+        return false;
+    }
+};
