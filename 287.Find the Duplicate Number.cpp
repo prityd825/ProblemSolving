c1c3fda26f228e4e1d348c0c6c77e2b61e2317ee
Prity Das <prityd825@gmail.com>
2024-03-24 19:30:40 +0600

    leetcode

---
+++ 287.Find the Duplicate Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int low = 1, high = nums.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+            if (count > mid) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
